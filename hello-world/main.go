700a5103803ab0124a5941e79ede8ec2f76b572e
mickaelyoshua7674 <mickaelyoshua@gmail.com>
2025-02-19 10:17:35 -0300

    practicing

---
+++ hello-world/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Count struct {
+	Count int
+}
+
+func main() {
+	count := Count{Count: 0}
+	router := gin.Default()
+
+	router.LoadHTMLGlob("views/*.html")
+
+	router.GET("/", func(c *gin.Context) {
+		count.Count++
+		c.HTML(http.StatusOK, "index", count) // Using index and not index.html because is identifying the block, not the file
+	})
+
+	router.Run(":8080")
+}
