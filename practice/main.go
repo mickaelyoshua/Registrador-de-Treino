2588d7094de878630424255f9976c70526fb1568
mickaelyoshua7674 <mickaelyoshua@gmail.com>
2025-02-20 13:26:00 -0300

    deleting

--- practice/main.go
+++ practice/main.go
@@ -2,16 +2,20 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
+var id = 0
 type Contact struct {
 	Name string
 	Email string
+	Id int
 }
 func newContact(name, email string) Contact {
-	return Contact{Name: name, Email: email}
+	id++
+	return Contact{Name: name, Email: email, Id: id}
 }
 type Contacts = []Contact
 
@@ -23,6 +27,14 @@ func newData() Data {
 		newContact("John", "jd@gmail.com"),
 		newContact("Clara", "cd@gmail.com"),
 	}}
+}
+func (d *Data) indexOf(id int) int {
+	for i, contact := range d.Contacts {
+		if contact.Id == id {
+			return i
+		}
+	}
+	return -1
 }
 func (d *Data) hasEmail(email string) bool {
 	for _, contact := range d.Contacts {
@@ -58,13 +70,13 @@ func main() {
 
 	router.LoadHTMLGlob("views/*.html")
 
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index", page) // Using index and not index.html because is identifying the block, not the file
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "index", page) // Using index and not index.html because is identifying the block, not the file
 	})
 
-	router.POST("/contacts", func(c *gin.Context) {
-		name := c.Request.FormValue("name")
-		email := c.Request.FormValue("email")
+	router.POST("/contacts", func(ctx *gin.Context) {
+		name := ctx.Request.FormValue("name")
+		email := ctx.Request.FormValue("email")
 
 		if page.Data.hasEmail(email) {
 			formData := newFormData()
@@ -72,12 +84,30 @@ func main() {
 			formData.Values["email"] = email
 			formData.Errors["email"] = "Email already exists"
 
-			c.HTML(http.StatusBadRequest, "form", formData)
+			ctx.HTML(http.StatusUnprocessableEntity, "form", formData)
 		}
 
-		page.Data.Contacts = append(page.Data.Contacts, newContact(name, email))
+		contact := newContact(name, email)
+		page.Data.Contacts = append(page.Data.Contacts, contact)
 
-		c.HTML(http.StatusOK, "display", page)
+		ctx.HTML(http.StatusOK, "form", newFormData())
+		ctx.HTML(http.StatusOK, "oob-contact", contact)
+	})
+
+	router.DELETE("/contacts/:id", func(ctx *gin.Context) {
+		idString := ctx.Param("id")
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			ctx.String(http.StatusBadRequest, "Invalid Id")
+		}
+
+		index := page.Data.indexOf(id)
+		if index == -1 {
+			ctx.String(http.StatusNotFound, "Contact not found")
+		}
+		page.Data.Contacts = append(page.Data.Contacts[:index], page.Data.Contacts[index+1:]...)
+
+		ctx.Status(http.StatusOK)
 	})
 
 	router.Run(":8080")
