5195f785e8796af460f0c910cb3e123fbd65d5ce
Mickael Yoshua <mickaelyoshua@gmail.com>
2025-04-22 13:01:49 +0000

    start again

--- app/main.go
+++ app/main.go
@@ -1,17 +1,5 @@
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-	"github.com/mickaelyoshua7674/Registrador-de-Treino/handlers"
-)
-
 func main() {
-	server := gin.Default()
-	server.LoadHTMLGlob("views/*.html")
 
-	server.GET("/register", handlers.Register)
-	server.GET("/login", handlers.Login)
-	server.GET("/", handlers.DefaultHandler)
-
-	server.Run(":8080")
 }
