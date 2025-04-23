47d73709c443ebe7f7c791bb0d1082abaa3381b0
mickaelyoshua7674 <mickaelyoshua@gmail.com>
2025-04-23 16:56:48 -0300

    inital files to hash password and jwt

--- app/util/util.go
+++ app/util/util.go
@@ -1,5 +1,60 @@
 package util
 
+import (
+	"time"
+	"errors"
+
+	"golang.org/x/crypto/bcrypt"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+// About password
 func ValidatePassword(pass, confirmPass string) bool {
 	return pass == confirmPass
+}
+
+func HashPassword(pass string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(pass), 14)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(pass, hashedPass string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPass), []byte(pass))
+	return err==nil
+}
+
+const secretKey = "supersecret"
+
+func GenerateToken(username string, id string) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"username": username,
+		"id": id,
+		"expiration": time.Now().Add(time.Hour*2).Unix(),
+	})
+	return token.SignedString([]byte(secretKey))
+}
+
+func VerifyToken(token string) (string, error) {
+	parsedToken, err := jwt.Parse(token, func(token *jwt.Token) (any, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return "", errors.New("unexpectd signing method")
+		}
+		return []byte(secretKey), nil
+	})
+	if err != nil {
+		return "", err
+	}
+
+	if !parsedToken.Valid {
+		return "", errors.New("invalid token")
+	}
+
+	claims, ok := parsedToken.Claims.(jwt.MapClaims)
+	if !ok {
+		return "", errors.New("invalid token claims")
+	}
+
+	id := string(claims["id"])
+	return id, nil
 }
